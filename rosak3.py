e29215b6d4d6b469a018d5ea228ebb957d4699f7
fatonah25 <firmanfatonah158@gmail.com>
2025-05-10 13:23:06 +0700

    Create rosak3.py

---
+++ rosak3.py
@@ -0,0 +1,45 @@
+from seleniumwire import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import time
+
+options = {
+    'proxy': {
+        'http': 'http://bcdffdd3446549c1b487__cr.sa:fa35d3e25afda39d@gw.dataimpulse.com:10023',
+        'https': 'https://bcdffdd3446549c1b487__cr.sa:fa35d3e25afda39d@gw.dataimpulse.com:10023',
+        'no_proxy': 'localhost,127.0.0.1'
+    }
+}
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--verbose")
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument("--window-size=1920, 1200")
+# Hapus '--headless' untuk melihat apakah ekstensi berjalan dengan benar dalam mode normal
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--disable-background-networking")  # Mencegah koneksi jaringan di latar belakang
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument("--disable-client-side-phishing-detection")  # Nonaktifkan deteksi phishing
+chrome_options.add_argument("--disable-default-apps")  # Nonaktifkan aplikasi bawaan Chrome
+chrome_options.add_argument("--disable-features=NetworkPrediction")  # Nonaktifkan prediksi jaringan
+chrome_options.add_argument("--disable-sync")  # Nonaktifkan sinkronisasi
+chrome_options.add_argument("--metrics-recording-only")  # Nonaktifkan pengumpulan data
+chrome_options.add_argument("--safebrowsing-disable-auto-update")  # Nonaktifkan pembaruan otomatis Safe Browsing
+chrome_options.add_argument("--disable-component-update")  # Nonaktifkan pembaruan komponen
+chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keandalan domain
+
+driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
+
+driver.get("https://sepolia-faucet.pk910.de/#/mine/de188429-bae8-4af0-9dfb-485a24c8ff65")
+time.sleep(50000)
+
+div_element = driver.find_element(By.CLASS_NAME, "col-3")
+content_text = div_element.text
+print(content_text)
+
+#WAKTU MENUNGGU MINING SELESAI
+time.sleep(15000)
+
+# Tutup browser
+driver.quit()
