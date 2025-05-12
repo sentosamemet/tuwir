d47e0e529f7c2c15db957ca24971a243f81ceef4
fatonah25 <firmanfatonah158@gmail.com>
2025-05-13 01:16:54 +0700

    Update rosak19.py

--- rosak19.py
+++ rosak19.py
@@ -7,8 +7,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://4d22aa6fac6758e39fa4__cr.au:974dc332f5e0ae67@gw.dataimpulse.com:10019',
-        'https': 'https://4d22aa6fac6758e39fa4__cr.au:974dc332f5e0ae67@gw.dataimpulse.com:10019',
+        'http': 'http://1b552cc6ab4ad260c24c__cr.au:a1a384bcb5a733ba@gw.dataimpulse.com:10019',
+        'https': 'https://1b552cc6ab4ad260c24c__cr.au:a1a384bcb5a733ba@gw.dataimpulse.com:10019',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
@@ -33,7 +33,7 @@ chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keand
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/205384e2-ad6b-4b45-94e5-ceea946215d9")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/eccaf857-85a1-417e-902a-184e155d9de3")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
