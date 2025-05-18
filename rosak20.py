7319a8c88538f1fb1ac2a4eee109efa0562304fa
fatonah25 <firmanfatonah158@gmail.com>
2025-05-18 21:03:31 +0700

    Update rosak20.py

--- rosak20.py
+++ rosak20.py
@@ -32,7 +32,7 @@ chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keand
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/a4c2b641-5178-482a-a3d3-ad9bbf0f252d")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/dd6ce5dd-ab37-416b-ab3a-10efe883d214")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
