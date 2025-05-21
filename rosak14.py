ff7bf1950c178257e66c3fad651eb28cef40d10a
fatonah25 <firmanfatonah158@gmail.com>
2025-05-21 07:52:57 +0700

    Update rosak14.py

--- rosak14.py
+++ rosak14.py
@@ -7,8 +7,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://97df29604bf964768223__cr.cl:05e84a3115febf6d@gw.dataimpulse.com:10014',
-        'https': 'https://97df29604bf964768223__cr.cl:05e84a3115febf6d@gw.dataimpulse.com:10014',
+        'http': 'http://30f229fee2c14d9ad3c4__cr.cl:abb79949fe6ee6ad@gw.dataimpulse.com:10014',
+        'https': 'https://30f229fee2c14d9ad3c4__cr.cl:abb79949fe6ee6ad@gw.dataimpulse.com:10014',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
