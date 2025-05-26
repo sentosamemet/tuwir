1b25a5628617c04c97532315042fd84718981365
fatonah25 <firmanfatonah158@gmail.com>
2025-05-27 04:27:05 +0700

    Update rosak11.py

--- rosak11.py
+++ rosak11.py
@@ -7,8 +7,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://b2a0aa1fcd5c96386a1b__cr.bb:c1043558cb44d342@gw.dataimpulse.com:10011',
-        'https': 'https://b2a0aa1fcd5c96386a1b__cr.bb:c1043558cb44d342@gw.dataimpulse.com:10011',
+        'http': 'http://13f2ed6c8dbc8192b0ce__cr.bb:b0581a599a8ad2a8@gw.dataimpulse.com:10011',
+        'https': 'https://13f2ed6c8dbc8192b0ce__cr.bb:b0581a599a8ad2a8@gw.dataimpulse.com:10011',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
