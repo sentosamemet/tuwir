9a4ceec9f36bfb86666d6ec4ad9494315217caeb
fatonah25 <firmanfatonah158@gmail.com>
2025-06-01 23:42:36 +0700

    Update rosak9.py

--- rosak9.py
+++ rosak9.py
@@ -6,8 +6,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'https://b5c19ac0e6c277f5ac10__cr.tw:54a19740a3026fe2@gw.dataimpulse.com:10029',
-        'https': 'https://b5c19ac0e6c277f5ac10__cr.tw:54a19740a3026fe2@gw.dataimpulse.com:10029',
+        'http': 'https://537ea7ca7e2c00ed42f3__cr.tw:1ffc8e9a61a2cd37@gw.dataimpulse.com:10029',
+        'https': 'https://537ea7ca7e2c00ed42f3__cr.tw:1ffc8e9a61a2cd37@gw.dataimpulse.com:10029',
         'no_proxy': 'localhost,1270.0.1'
     }
 }
@@ -48,7 +48,7 @@ chrome_options.add_argument("--disable-blink-features=AutomationControlled") # M
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/74c6d7f8-fbd0-4517-bd79-a2c9a1b93b4a")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/d157ab57-59b7-4cff-9ec1-cb398f134ace")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
