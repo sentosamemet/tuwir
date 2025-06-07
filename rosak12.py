813bd3e54bd8a56ad4f3e36073414058fbe3b555
fatonah25 <firmanfatonah158@gmail.com>
2025-06-08 06:57:17 +0700

    Update rosak12.py

--- rosak12.py
+++ rosak12.py
@@ -49,7 +49,7 @@ chrome_options.add_argument("--disable-blink-features=AutomationControlled") # M
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/86d39e19-8850-48d4-bdbf-32137dc6982a")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/2d752f74-f083-4845-aafa-7e40e4db9879")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
