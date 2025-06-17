20dd7a2d7ff19553725fc71197e2d31e631825fd
sentosamemet <sentosamemet@gmail.com>
2025-06-17 21:44:49 +0700

    Update rosak8.py

--- rosak8.py
+++ rosak8.py
@@ -6,8 +6,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://2766f184a8ea3245eeeb__cr.th:c586aaf043d69e45@gw.dataimpulse.com:10008',
-        'https': 'https://2766f184a8ea3245eeeb__cr.th:c586aaf043d69e45@gw.dataimpulse.com:10008',
+        'http': 'http://79116e362a3f6b21cf7f__cr.th:6dcd35a068d29504@gw.dataimpulse.com:10008',
+        'https': 'https://79116e362a3f6b21cf7f__cr.th:6dcd35a068d29504@gw.dataimpulse.com:10008',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
