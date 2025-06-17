54c2150be749b9fc08d5bf2c99b1b2d91e1da9c0
sentosamemet <sentosamemet@gmail.com>
2025-06-17 21:45:25 +0700

    Update rosak14.py

--- rosak14.py
+++ rosak14.py
@@ -7,8 +7,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://2766f184a8ea3245eeeb__cr.cl:c586aaf043d69e45@gw.dataimpulse.com:10014',
-        'https': 'https://2766f184a8ea3245eeeb__cr.cl:c586aaf043d69e45@gw.dataimpulse.com:10014',
+        'http': 'http://79116e362a3f6b21cf7f__cr.cl:6dcd35a068d29504@gw.dataimpulse.com:10014',
+        'https': 'https://79116e362a3f6b21cf7f__cr.cl:6dcd35a068d29504@gw.dataimpulse.com:10014',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
